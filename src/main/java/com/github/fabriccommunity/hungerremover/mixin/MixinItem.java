a469519903cbda6dae37f6f794b5bb8adc12f69b
MrIbby <siribby@outlook.com>
2019-07-24 16:56:07 -0700

    Add mod code

---
+++ src/main/java/com/github/fabriccommunity/hungerremover/mixin/MixinItem.java
@@ -0,0 +1,19 @@
+package com.github.fabriccommunity.hungerremover.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public class MixinItem {
+    @Inject(at = @At("HEAD"), method = "getMaxUseTime", cancellable = true)
+    private void onGetMaxUseTime(ItemStack itemStack, CallbackInfoReturnable<Integer> info) {
+        if (itemStack.getItem().isFood()) {
+            // Food items don't work with 0, so 1 is the next best thing.
+            info.setReturnValue(1);
+        }
+    }
+}
